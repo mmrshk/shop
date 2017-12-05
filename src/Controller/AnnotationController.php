48eaa275d90bfd2d29274ae21c7c17530181c735
mmrshk <emma.yeroshek@gmail.com>
2017-12-05 19:13:35 +0200

    Annotation+

--- src/Controller/AnnotationController.php
+++ src/Controller/AnnotationController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class AnnotationController extends Controller
 {
     /**
@@ -20,6 +21,6 @@ class AnnotationController extends Controller
      */
     public function route()
     {
-        return "Some information";
+        return $this->render('lucky\route.html.twig');
     }
 }
